06e2b8368c527bca23c580e990efe1b6ffb83dab
nbaugher2 <nbaugher2@gmail.com>
2023-09-18 20:09:59 -0400

    calculator functions
    basic arithmetic functions:
    add
    subtract
    multiply
    divide

---
+++ source.js
@@ -0,0 +1,19 @@
+//used for calculator addition
+function add(a,b){ 
+	return a + b;
+}
+
+//used for calculator subtraction
+function sub(a,b){ 
+	return a - b;
+}
+
+//used for calculator multiplication
+function mul(a,b){
+	return a * b;
+}
+
+//used for calculator division
+function div(a,b){
+	return a / b;
+}
