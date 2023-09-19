037e37ecc275627d058aa35221a11fa56b610b99
nbaugher2 <nbaugher2@gmail.com>
2023-09-18 20:17:44 -0400

    operate function
    operate wrapper function for arithmetic functions

--- source.js
+++ source.js
@@ -1,3 +1,8 @@
+//variables used by calculator
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+
 //used for calculator addition
 function add(a,b){ 
 	return a + b;
@@ -16,4 +21,15 @@ function mul(a,b){
 //used for calculator division
 function div(a,b){
 	return a / b;
+}
+
+//takes in two numbers and applies operator op
+function operate(a, b, op){
+	if (op === "+") return add(a,b);
+
+	if (op === "-") return sub(a,b);
+
+	if (op === "*") return mul(a,b);
+
+	if (op === "/") return div(a,b);
 }
